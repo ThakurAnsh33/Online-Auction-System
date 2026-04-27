d44ded89217288e49c9f0fe061a9e87b6c567e56
rajput798ansh <anshmvm@gmail.com>
2026-04-27 19:30:55 +0530

    chore: harden deploy startup and split frontend bundles

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,4 +9,15 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom", "react-router-dom"],
+          redux: ["@reduxjs/toolkit", "react-redux"],
+          charts: ["chart.js", "react-chartjs-2"],
+        },
+      },
+    },
+  },
 })
